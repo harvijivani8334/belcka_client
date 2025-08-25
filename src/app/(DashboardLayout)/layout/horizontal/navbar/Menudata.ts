4c76193f8c65b5330839297c5102f67c12a06d74
urvish <urvishgodhani8549@gmail.com>
2025-08-25 16:01:58 +0530

    build changes

--- src/app/(DashboardLayout)/layout/horizontal/navbar/Menudata.ts
+++ src/app/(DashboardLayout)/layout/horizontal/navbar/Menudata.ts
@@ -2,17 +2,19 @@ import { IconFiles, IconHome } from "@tabler/icons-react";
 import { uniqueId } from "lodash";
 
 const Menuitems = [
-   // {
-   //   id: uniqueId(),
-   //   title: "Dashboard",
-   //   icon: IconChartPie,
-   //   href: "/",
-   // },
-    {
-     id: uniqueId(),
-     title: "Projects",
-     icon: IconFiles,
-     href: "/apps/projects/index",
-   },
+  {
+    id: uniqueId(),
+    title: "Dashboard",
+    icon: IconHome,
+    href: "/",
+    children: [
+      {
+        id: uniqueId(),
+        title: "Projects",
+        icon: IconFiles,
+        href: "/apps/projects/index",
+      },
+    ],
+  },
 ];
 export default Menuitems;
